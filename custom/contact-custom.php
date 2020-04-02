029bdb9e20ce033a3b41955ecf4569c09c722f80
Coldfire477 <mc-meca@hotmail.fr>
2020-04-02 12:52:59 +0200

    Formulaire de contact mise en place + fin de traductrion

--- custom/contact-custom.php
+++ custom/contact-custom.php
@@ -9,29 +9,10 @@
 
 <?php get_header() ?>
 
-
-
-
-<div class="content ml-2 mt-4 mb-4">
-            <p class="h3">Contact us</p>
-            <form action="" method="POST" class="mt-4 ml-4">
-                <div class="form-group">
-                  <label for="exampleFormControlInput1">Email</label>
-                  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">Object</label>
-                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Why do you contact us">
-                  </div>
-                <div class="form-group">
-                  <label for="exampleFormControlTextarea1">Your message</label>
-                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
-                </div>
-                <button class="btn btn-primary" type="submit">Send</button>
-              </form>
-              <p class="p-3 mb-2 ml-4 bg-danger text-white"><?php _e('NOTE: A contact form such as this would require some way of emailing the input to an email address. In a close futur this won\'t be necessary but for now you\'ll have to install a form extension', 'justgreat') ?></p>
-        </div>
-
-
+<?php while (have_posts()) : the_post(); ?>
+<p class="h3 mt-4 ml-4"><?php _e('Contact us', 'justgreat') ?></p>
+    <?php the_content() ?>
+    <div class="clear"></div>
+<?php endwhile; wp_reset_query();?>
 
 <?php get_footer() ?>
--- page.php
+++ page.php
@@ -1,22 +1,7 @@
 <?php get_header() ?>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<?php while (have_posts()) : the_post(); ?>
+    <?php the_content() ?>
+<?php endwhile; wp_reset_query();?>
 
 <?php get_footer()?>
